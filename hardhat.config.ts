19ea38381f9a41a988455ef19232d81ba80cbdd5
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2022-07-20 14:43:36 +0900

    set network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_RPC_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
