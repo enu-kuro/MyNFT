12ec83d3109ac955fa9ad1c26929fa8fd76c0f07
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2022-07-21 14:58:41 +0900

    mint script

---
+++ scripts/mint.ts
@@ -0,0 +1,25 @@
+import { ethers } from "hardhat";
+
+const CONTRACT_ADDRESS = "0xb9Aa10890b019AF260dF83740bD36cDf042209FB";
+const BAYC_TOKEN_URI =
+  "https://bafybeihpjhkeuiq3k6nqa3fkgeigeri7iebtrsuyuey5y6vy36n345xmbi.ipfs.dweb.link/2";
+
+const STEPN_TOKEN_URI = "https://api.stepn.com/run/nftjson/101/96528793170";
+
+async function main() {
+  const [owner] = await ethers.getSigners();
+  const myNft = await ethers.getContractAt("MyNFT", CONTRACT_ADDRESS);
+
+  const mintTx = await myNft.mintNFT(owner.address, BAYC_TOKEN_URI);
+  await mintTx.wait();
+
+  const balance = await myNft.balanceOf(owner.address);
+  console.log(balance);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
